30df703a78d56e092916ba6c6f84cf7b6c5c99d3
Giulio Troccoli-Allard <giulio@troccoli.it>
2021-05-17 18:25:36 +0100

    Hidden unused (yet) option on the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     </x-slot>
 
     <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-        <div class="relative">
+        <div class="relative hidden">
             <dt>
                 <button
                     class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
@@ -32,7 +32,7 @@
             </dd>
         </div>
 
-        <div class="relative">
+        <div class="relative hidden">
             <dt>
                 <button
                     class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
