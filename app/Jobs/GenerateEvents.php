af0c162ba10abe579a9ec42ffc54e61d1616535d
Giulio Troccoli-Allard <giulio@troccoli.it>
2021-05-27 09:52:40 +0100

    Prevented adding Google events for holidays

--- app/Jobs/GenerateEvents.php
+++ app/Jobs/GenerateEvents.php
@@ -4,12 +4,14 @@ namespace App\Jobs;
 
 use App\DTOs\Duty;
 use App\Events\EventsGenerated;
+use App\Models\Holiday;
 use App\Models\Job;
 use App\Models\ShiftEvent;
 use App\Services\Rota;
 use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
@@ -43,15 +45,19 @@ class GenerateEvents implements ShouldQueue
 
     private function generateEvents(Rota $service): void
     {
+        $holidays = Holiday::query()->between($this->eventsJob->getStart(), $this->eventsJob->getEnd())->get();
         $currentDate = $this->eventsJob->getStart()->clone();
         $position = $this->eventsJob->getPosition();
         $lastPosition = $service->getNumberOfPositions();
         while ($currentDate->isBefore($this->eventsJob->getEnd())) {
             $week = $service->getWeek($position);
-            if ($week->isLeaveCover()) {
+            if ($this->isOnHoliday($holidays, $currentDate)) {
+                $currentDate->addWeek();
+                $this->eventsJob->incrementNumberOfShiftsGenerated(7);
+            } elseif ($week->isLeaveCover()) {
                 ShiftEvent::createAllDayShiftEvent(
                     $this->eventsJob->getId(),
-                    self::EVENT_PREFIX . ' (Leave Cover)',
+                    self::EVENT_PREFIX.' (Leave Cover)',
                     $currentDate,
                     $currentDate->clone()->addWeek()->subDay()
                 );
@@ -99,5 +105,17 @@ class GenerateEvents implements ShouldQueue
         }
 
         return [$startDateTime, $endDateTime];
+    }
+
+    private function isOnHoliday(Collection $holidays, Carbon $date): bool
+    {
+        /** @var Holiday $holiday */
+        foreach ($holidays as $holiday) {
+            if ($date->greaterThanOrEqualTo($holiday->getStart()) && $date->lessThan($holiday->getStart()->clone()->addWeeks(2))) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
